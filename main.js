eee47bf65a86c4279ed558c7c7c2b3a6d6736a72
Michael Schmidt <lavisrap@gmail.com>
2014-08-03 10:21:55 +0200

    test

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 cc.game.onStart = function(){
-    cc.view.setDesignResolutionSize(320, 576, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(640, 1152, cc.ResolutionPolicy.SHOW_ALL);
 	cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new MuprisScene());
+        cc.director.runScene(new _42Scene());
     }, this);
 };
+if( !Math.sign ) Math.sign = function(val) {
+	return val < 0? -1:1;
+}
 cc.game.run();
