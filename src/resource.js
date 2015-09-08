d8b3790a7a32e47bfb40e2ba8b2c3fcec70c019b
dieterschwartz <schwartz-dieter@t-online.de>
2015-09-08 23:32:31 +0100

    res blue quadrat

--- src/resource.js
+++ src/resource.js
@@ -52,6 +52,7 @@ var res = {
     red_hills_delete_row_mp3:         "res/sounds/levelsounds/rote_huegel_reihe_kracht0,1,7.mp3",
     red_hills_delete_last_rows_mp3:   "res/sounds/levelsounds/rote_huegel_reihe_kracht0,1,7.mp3",
     red_hills_final_mp3:              "res/sounds/levelsounds/rote_huegel_schluss_fanfare1,1,5.mp3",
+
     // level 2
     flames_intro_mp3:                  "res/sounds/levelsounds/flammen_hintergrund_einleitung0,1,9.mp3",
     flames_loop_mp3:                   "res/sounds/levelsounds/flammen_hintergrund_loop0,1,9.mp3",
@@ -71,6 +72,33 @@ var res = {
     flames_delete_row_mp3:             "res/sounds/levelsounds/flammen_reihe_kracht0,1,7.mp3",
     flames_delete_last_rows_mp3:        "res/sounds/levelsounds/flammen_reihe_kracht0,1,7.mp3",
     flames_final_mp3:                  "res/sounds/levelsounds/flammen_reihe_kracht0,1,7.mp3",
+
+    // level 5
+    //blue_quadrat_intro_mp3:                  "res/sounds/none.mp3",
+    blue_quadrat_loop_mp3:                   "res/sounds/levelsounds/L5_blaues_quadrat_hintergrund_loop1,1,2.mp3",
+    blue_quadrat_level_words_mp3:            "res/sounds/levelsounds/L5_blaues_quadrat_wort_fertig_b_1,1,2.mp3",
+    blue_quadrat_level_nr_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_letztes_wort_1,1,2.mp3",
+    blue_quadrat_set_tile_1_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_oben_a_1,1,2.mp3",
+    blue_quadrat_set_tile_2_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_oben_b_1,1,2.mp3",
+    blue_quadrat_set_tile_3_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_oben_c_1,1,2.mp3",
+    //blue_quadrat_swipe_1_mp3:                "res/sounds/none.mp3",
+    //blue_quadrat_swipe_2_mp3:                "res/sounds/none.mp3",
+    //blue_quadrat_swipe_3_mp3:                "res/sounds/none.mp3",
+    blue_quadrat_rotate_1_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_drehen_a_1,1,2.mp3",
+    blue_quadrat_rotate_2_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_drehen_b_1,1,2.mp3",
+    blue_quadrat_rotate_3_mp3:               "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_drehen_c_1,1,2.mp3",
+    blue_quadrat_fix_tile_1_mp3:             "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_unten_a_1,1,2.mp3",
+    blue_quadrat_fix_tile_2_mp3:             "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_unten_b_1,1,2.mp3",
+    blue_quadrat_fix_tile_3_mp3:             "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_unten_c_1,1,2.mp3",
+    blue_quadrat_selection_1_mp3:              "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_kombi_a_1,1,2.mp3",
+    blue_quadrat_selection_2_mp3:              "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_kombi_b_1,1,2.mp3",
+    blue_quadrat_selection_3_mp3:              "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_kombi_c_1,1,2.mp3",
+    blue_quadrat_full_word_1_mp3:              "res/sounds/levelsounds/L5_blaues_quadrat_wort_fertig_a_1,1,2.mp3",
+    blue_quadrat_full_word_2_mp3:              "res/sounds/levelsounds/L5_blaues_quadrat_wort_fertig_b_1,1,2.mp3",
+    blue_quadrat_last_word_mp3:              "res/sounds/L5_blaues_quadrat_wort_fertig_a_1,1,2.mp3",
+    blue_quadrat_delete_row_mp3:             "res/sounds/levelsounds/L5_blaues_quadrat_reihe_kracht_a_1,1,2.mp3",
+    blue_quadrat_delete_last_rows_mp3:        "res/sounds/levelsounds/L5_blaues_quadrat_buchstaben_unten_a_1,1,2.mp3",
+    blue_quadrat_final_mp3:                  "res/sounds/levelsounds/L5_blaues_quadrat_letztes_wort_1,1,2.mp3",
 
     // level 6
     inka_temple_intro_a_mp3:           "res/sounds/levelsounds/inka_tempel_hintergrund_a1,1,2.mp3",
