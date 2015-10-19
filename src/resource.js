4e1d77ef483be5b7914f1df168205766ad76c835
dieterschwartz <schwartz-dieter@t-online.de>
2015-10-19 03:00:09 +0100

    delete old files

--- src/resource.js
+++ src/resource.js
@@ -210,7 +210,7 @@ var res = {
     blue_mountains_loop_b_mp3:          "res/sounds/levelsounds/blau_berge_loop_b_2,2,1.mp3",
     blue_mountains_level_words_mp3:     "res/sounds/levelsounds/blue_mountain_level_words_1,1,2.mp3",
     blue_mountains_level_nr_mp3:        "res/sounds/levelsounds/blue_mountain_level_nr_1,1,2.mp3",
-    blue_mountains_set_tile_mp3:        "res/sounds/levelsounds/blaue_berge_buchstaben_neu0,1,5.mp3",
+    blue_mountains_set_tile_mp3:        "res/sounds/levelsounds/blaue_berge_buchstaben_neu0,1,6.mp3",
     blue_mountains_swipe_mp3:           "res/sounds/levelsounds/blaue_berge_wischen0,1,17.mp3",
     blue_mountains_rotate_1_mp3:        "res/sounds/levelsounds/blaue_berge_drehen1,1,9.mp3",
     blue_mountains_rotate_2_mp3:        "res/sounds/levelsounds/blaue_berge_drehen2,1,9.mp3",
